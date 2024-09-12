88012509f6894acba7eae5ee4d2529d1a4bd3505
Majd833 <lina.azzam.1611@gmail.com>
2024-09-12 18:56:05 +0300

    grades

---
+++ grades.py
@@ -0,0 +1,7 @@
+math=int(input("Math:"))
+science=int(input("science:"))
+English=int(input("english:"))
+geography=int(input("geography:"))
+sum=math+science+English+geography
+average=(sum/400)*100
+print("The average is :",average)
